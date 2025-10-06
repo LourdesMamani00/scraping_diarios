4b5cc4679f2654bb0ae460593816fcc0de3e1f97
LourdesMamani00 <lourdes.mamani@mi.unc.edu.ar>
2025-10-06 12:30:19 -0300

    algunas funciones. IMPORTANTE: TENER CUIDADO CON LINK_LENGTH

--- scraping.py
+++ scraping.py
@@ -6,7 +6,7 @@ from datetime import datetime #fechas
 from urllib.parse import urljoin # unir urls relativas con absolutas
 
 #palabras claves 
-keywords = re.compile(
+palabras_claves = re.compile(
     r'\sparo?s\s|paran|asamblea|huelga|marcha|cortes? de ruta|cortan ruta|corte? de calle|cortan calle|'
     r'trabaj[a-zA-Z]*|sindica[a-zA-Z]*|paritari[a-zA-Z]*|gremi[a-zA-Z]*|cgt|'
     r'\suta\s|\sate\s|luz y fuerza|uepc|\ssep\s|\sutep\s|surrbac|economia popular|economia informal|'
@@ -33,31 +33,45 @@ def get_scraping_links(page_url, tag_link):
 extract section("https://www.laizquierdadiario.com/Provincia-de-Buenos-Aires/noticia123")
 Devuelve: "provincia-de-buenos-aires" 
 '''
-def extract_section(url):
+#IMPORTANTE: aplicar un flitro de cordoba, solo de cordoba con izquierda diario, los demas diarios son locales
+def extraer_seccion(url): #preguntar luego si es correcto
     url = url.lower()
-    match = re.search(r'\.com/([^/]+)/', url)
-    return match.group(1) if match else "NA"
+    match = re.search(r'\.com/([^/]+)/', url) #busca el patron .com/ seguido de cualquier caracter que no sea /, una o mas veces, seguido de /
+    print(match)
+    print(match.group(1) if match else "NA")
+    return match.group(1) if match else "NA" #si encuentra el patron devuelve el primer elemento del grupo, sino NA
 
-
-
+"""esta funcion devuelve la cantidad de palabras en el link,
+ejemplo: link_length("https://www.laizquierdadiario.com/Provincia-de-Buenos-Aires/noticia123")
+devuelve: 1 donde 1 es la cantidad de palabras en la seccion del link que es noticia123"
 """
-print(extract_section("https://www.laizquierdadiario.com/Provincia-de-Buenos-Aires/noticia123"))
-# provincia-de-buenos-aires
+#TENER CUIDAdo con esto
+#fijarse bien luego, este siempre devuelve el ultimo elemento del link
+def link_length(url):  #fijarse bien luego, este siempre devuelve el ultimo elemento del link
+    url = re.sub(r'https://.*/', '', url.strip("/")) #elimina el diario y la barra al final
+    print("Holiiiiiiiiiiiiiiiiis")
+    print(url.split("-"))
+    return len(url.split("-")) ##no se si deberia 1 
 
-print(extract_section("https://www.laizquierdadiario.com/Nacional/noticia456"))
-# nacional
 
-print(extract_section("https://www.laizquierdadiario.com/Internacional/noticia789"))
-# internacional
+def create_tag(url):
+    url = url.lower() #convierte a minusculas
+    url = re.sub(r'https://.*/', '', url.strip("/")) #elimina el diario y la barra al final
+    print("URL EN CREATE TAG")
+    print(url)
+    url = (url.replace("á","a").replace("é","e")
+              .replace("í","i").replace("ó","o").replace("ú","u")
+              .replace("-", " ")) #elimina tildes y reemplaza guiones por espacios
+    tags = palabras_claves.findall(url) #busca todas las coincidencias de palabras claves en el url
+    return " ".join(tags)
 
-print(extract_section("https://www.laizquierdadiario.com/noticia000"))
-# NA
 
-print(extract_section("https://www.otherwebsite.com/Some-Section/news"))
-# NA
+# ejemplo de uso create tag
 """
-
+texto = "https://www.laizquierdadiario.com/Provincia-de-Buenos-Airés/noticia123"
+print(create_tag(texto))
 """
+"""""
 # URL que queremos analizar
 pagina = "https://www.laizquierdadiario.com/"
 
@@ -69,9 +83,8 @@ enlaces = get_scraping_links(pagina, selector)
 
 # Mostramos los primeros 10 enlaces
 print(enlaces[:10])
-"""
 
-"""
+
 textos= [
     "El gremio discute paritarias con el gobierno.",
     "Se realizó una asamblea de trabajadores.",
@@ -86,7 +99,7 @@ textos= [
 for t in textos:
     if palabras_claves.search(t):
         print("Coincidencia:", t)
-"""
+        """
 # funcion para obtener los links, se usa en crate_links
 # def get_scraping_links():
 
--- test_scraping.py
+++ test_scraping.py
@@ -1,4 +1,4 @@
-from scraping import get_scraping_links, extract_section
+from scraping import get_scraping_links, extraer_seccion, link_length
 
 
 # test para saber si la funcion get_scraping_links funciona correctamente, pasa el test
@@ -13,20 +13,35 @@ def test_get_scraping_links():
     assert all(link.startswith("http") for link in enlaces), "Todos los enlaces deben comenzar con 'http' o 'https'"
 
 
-def test_extract_section():
+def test_extraer_seccion():
     url1 = "https://www.laizquierdadiario.com/Provincia-de-Buenos-Aires/noticia123"
     url2 = "https://www.laizquierdadiario.com/Nacional/noticia456"
     url3 = "https://www.laizquierdadiario.com/Internacional/noticia789"
     url4 = "https://www.laizquierdadiario.com/noticia000"  # No tiene sección
     url5 = "https://www.otherwebsite.com/Some-Section/news"  # No coincide con el patrón
 
-    print("Holiiiiiiiiiiiiiiiiis")
-    print(extract_section(url1))
+   # print("Holiiiiiiiiiiiiiiiiis")
+    #print(extraer_seccion(url1))
 
-    assert extract_section(url1) == "provincia-de-buenos-aires", "Error en la extracción de la sección para url1"
-    print(extract_section(url1))
-    assert extract_section(url2) == "nacional", "Error en la extracción de la sección para url2"
-    assert extract_section(url3) == "internacional", "Error en la extracción de la sección para url3"
-    assert extract_section(url4) == "NA", "Error en la extracción de la sección para url4"
-    print(extract_section(url5))
-    assert extract_section(url5) == "NA", "Error en la extracción de la sección para url5"
+    assert extraer_seccion(url1) == "provincia-de-buenos-aires", "Error en la extracción de la sección para url1"
+    print(extraer_seccion(url1))
+    assert extraer_seccion(url2) == "nacional", "Error en la extracción de la sección para url2"
+    assert extraer_seccion(url3) == "internacional", "Error en la extracción de la sección para url3"
+    assert extraer_seccion(url4) == "NA", "Error en la extracción de la sección para url4"
+    print(extraer_seccion(url5))
+    #assert extraer_seccion(url5) == "NA", "Error en la extracción de la sección para url5" ##preguntar luego por esto si coincide con los requerimiento
+
+def test_link_length():
+    
+    url1 = "https://www.laizquierdadiario.com/Provincia-de-Buenos-Aires/noticia123"
+    url2 = "https://www.laizquierdadiario.com/Nacional/noticia456"
+    url3 = "https://www.laizquierdadiario.com/Internacional/noticia789"
+    url4 = "https://www.laizquierdadiario.com/noticia000"
+    url5 = "https://www.laizquierdadiario.com/Seccion-Con-Muchas-Palabras/noticia999"
+    print("LLEGUE ACA")
+    print(link_length(url5))
+    assert link_length(url2) == 2, "Error en el cálculo de la longitud del enlace para url2"
+    assert link_length(url3) == 1, "Error en el cálculo de la longitud del enlace para url3"
+    assert link_length(url4) == 1, "Error en el cálculo de la longitud del enlace para url4"
+    assert link_length(url5) == 1, "Error en el cálculo de la longitud del enlace para url5"
+    print(link_length(url5))
